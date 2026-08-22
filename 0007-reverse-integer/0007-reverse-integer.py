c578ac682319a5d58e9c23c3d7a59f9c918b1eea
Subhranil Gupta <subhranilgupta07@gmail.com>
2026-08-22 10:55:42 +0530

    Time: 16 ms (69.67%), Space: 12.5 MB (14.19%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def reverse(self, x):
+        a = str(x)
+        b = a[::-1]
+        if a[0] == '-':
+            c = -int(b[0:len(a)-1])
+        else:
+            c = int(b)
+        if c < -2**31 or c > 2**31 - 1:
+            return 0
+
+        return c
