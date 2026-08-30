9b2f56a67243b06cdb205e759cf746f3198ca690
Subhranil Gupta <subhranilgupta07@gmail.com>
2026-08-30 11:34:11 +0530

    Time: 0 ms (100%), Space: 48.5 MB (13.99%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,27 @@
+class Solution 
+{
+    public int search(int[] nums, int target) 
+    {
+        int low = 0;
+        int high = nums.length - 1;
+        int pos = -1;
+        while (low <= high)
+        {
+            int mid = low + (high - low)/2;
+            if (nums[mid] == target)
+            {
+                pos = mid;
+                break;
+            }
+            else if (nums[mid] > target)
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return pos;
+    }
+}
